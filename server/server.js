4d31c8cee8c1326b2faff523a9634e943ec640f2
alexbrenes999 <116224156+alexbrenes999@users.noreply.github.com>
2023-04-27 01:12:14 -0400

    updated server

--- server/server.js
+++ server/server.js
@@ -1,21 +1,43 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+import express from 'express';
+import path from 'path';
+import db from './config/connection';
+import routes from './routes';
+import { typeDefs, resolvers } from './schemas';
+import { ApolloServer } from 'apollo-server-express';
+import { authMiddleware } from './utils/auth';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes);
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+  // context: ({ req }) => ({ req })
+});
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/public/index.html'));
+});
+
 app.use(routes);
 
-db.once('open', () => {
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+})
+};
+
+startApolloServer(resolvers, typeDefs);
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,10 +1,10 @@
-const jwt = require('jsonwebtoken');
+import jwt from 'jsonwebtoken';
 
 // set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
-module.exports = {
+const authStuff = {
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
@@ -37,3 +37,5 @@ module.exports = {
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
+
+export default authStuff;
